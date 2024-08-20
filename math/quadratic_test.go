17054945a98478b64abb8c93b6d5ece627c26e67
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-08-20 16:54:34 +0100

    added a math function

---
+++ math/quadratic_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
+func RootofEqn(a, b, c float64) (float64, float64) {
+	d := b*b - 4*a*c
+	root := math.Sqrt(d)
+	num1 := (-b + root) / 2 * a
+	num2 := (-b - root) / 2 * a
+
+	return num1, num2
+}
+
+func TestRootofEqn(t *testing.T) {
+	got1, got2 := RootofEqn(1, -2, 1)
+	want1, want2 := 1.0, 1.0
+
+	if got1 != want1 && got2 != want2 {
+		t.Errorf("expected %f and %f got %f and %f", want1, want2, got1, got2)
+	}
+}
+func main() {
+	root1, root2 := RootofEqn(1, -2, 1)
+
+	fmt.Printf("x is %f, y is %f", root1, root2)
+}
