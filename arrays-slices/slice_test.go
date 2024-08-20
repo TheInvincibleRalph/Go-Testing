7bde6a2ed001e64d2e89972d123d92c67237adce
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-08-20 13:12:55 +0100

    added SumAllTail

--- arrays-slices/slice_test.go
+++ arrays-slices/slice_test.go
@@ -50,6 +50,45 @@ func SumAll(numsToSum ...[]int) []int {
 
 }
 
+func SumAllTails(numsToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numsToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := Sum(numbers) - numbers[0] //or tail := numbers[1:]
+			sums = append(sums, tail)         //then sums := append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
+
+func TestSumAllTail(t *testing.T) {
+
+	assertEqual := func(t testing.TB, got, want []int) { //this is how to assign a function to a variable!
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("expected %v got %v", want, got)
+		}
+	}
+
+	t.Run("sums the tails of a slice", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3, 4, 5}, []int{6, 7, 8, 9, 10})
+		want := []int{14, 34}
+
+		assertEqual(t, got, want)
+	})
+
+	t.Run("safely sums empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1, 2, 3, 4})
+		want := []int{0, 9}
+
+		assertEqual(t, got, want)
+	})
+
+}
+
 func assert(t testing.TB, num []int, got, want int) {
 	t.Helper()
 	if got != want {
