738dfbd11c02ff1bc08e14778c863517525c7b80
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-08-22 00:43:05 +0100

    added intergration test

--- structs-interfaces/perimeter_test.go
+++ structs-interfaces/perimeter_test.go
@@ -30,24 +30,39 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
+	areaTest := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10.0, 15.0}, 150.0},
+		{Circle{10}, 314.1592653589793},
+	}
 
-		got := shape.Area()
-		if got != want {
-			t.Errorf("expected %g got %g", want, got)
+	for _, tt := range areaTest {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("expected %g got %g", got, tt.want)
 		}
 	}
-	t.Run("returns the area of a rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 20.0}
-		checkArea(t, rectangle, 200.0)
-	})
 
-	t.Run("returns the area of a circle", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
+	// checkArea := func(t testing.TB, shape Shape, want float64) {
+	// 	t.Helper()
 
-	})
+	// 	got := shape.Area()
+	// 	if got != want {
+	// 		t.Errorf("expected %g got %g", want, got)
+	// 	}
+	// }
+	// t.Run("returns the area of a rectangle", func(t *testing.T) {
+	// 	rectangle := Rectangle{10.0, 20.0}
+	// 	checkArea(t, rectangle, 200.0)
+	// })
+
+	// t.Run("returns the area of a circle", func(t *testing.T) {
+	// 	circle := Circle{10}
+	// 	checkArea(t, circle, 314.1592653589793)
+
+	// })
 }
 
 func Perimeter(rectangle Rectangle) float64 {
