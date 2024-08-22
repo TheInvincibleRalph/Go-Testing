452768b0ccea05b9829d9f2cec0f235fe9ec60fd
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-08-22 06:37:11 +0100

    ...

--- structs-interfaces/perimeter_test.go
+++ structs-interfaces/perimeter_test.go
@@ -14,6 +14,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 type Shape interface {
 	Area() float64
 }
@@ -34,8 +39,9 @@ func TestArea(t *testing.T) {
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{10.0, 15.0}, 150.0},
-		{Circle{10}, 314.1592653589793},
+		{Rectangle{Width: 10.0, Height: 15.0}, 150.0},
+		{Circle{Radius: 10}, 314.1592653589793},
+		{Triangle{Base: 12, Height: 6}, 36},
 	}
 
 	for _, tt := range areaTest {
@@ -77,8 +83,14 @@ func (c Circle) Area() float64 {
 	return math.Pi * (c.Radius * c.Radius)
 }
 
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
+
 // Methods are very similar to functions but they are called by invoking them on an instance of a particular type.
 // Where you can just call functions wherever you like, such as Area(rectangle) you can only call methods on "things".
 
 // Interfaces are a very powerful concept in statically typed languages like Go because they allow you to make
 // functions that can be used with different types and create highly-decoupled code whilst still maintaining type-safety.
+
+// If a type does not implement an interface, it means that type is not associated with the methods defined within the interface.
