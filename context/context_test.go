e626f66cf7725b43123d9eca7d9b73e3782d4b29
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-09-04 02:24:27 +0100

    added context

---
+++ context/context_test.go
@@ -0,0 +1,41 @@
+package context
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type Store interface {
+	Fetch() string
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, store.Fetch())
+	}
+}
+
+// Spy for store
+type SpyStore struct {
+	response string
+}
+
+func (s *SpyStore) Fetch() string {
+	return s.response
+}
+
+func TestServer(t *testing.T) {
+	data := "hello, world"
+	srv := Server(&SpyStore{data})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	srv.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf("expected %s got %s", data, response.Body.String())
+	}
+}
