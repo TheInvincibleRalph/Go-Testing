74dfdfd05d5b4871c0a4f750a8d82e5aa62a591f
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-09-04 02:38:45 +0100

    added context

--- context/context_test.go
+++ context/context_test.go
@@ -1,14 +1,17 @@
 package context
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 type Store interface {
 	Fetch() string
+	Cancel()
 }
 
 func Server(store Store) http.HandlerFunc {
@@ -19,23 +22,42 @@ func Server(store Store) http.HandlerFunc {
 
 // Spy for store
 type SpyStore struct {
-	response string
+	response  string
+	cancelled bool
 }
 
 func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
 	return s.response
 }
 
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
+
 func TestServer(t *testing.T) {
-	data := "hello, world"
-	srv := Server(&SpyStore{data})
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
 
-	srv.ServeHTTP(response, request)
+		data := "hello, world"
+		store := &SpyStore{response: data}
+		srv := Server(store)
 
-	if response.Body.String() != data {
-		t.Errorf("expected %s got %s", data, response.Body.String())
-	}
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		// Create a new context with a timeout of 5ms
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+
+		srv.ServeHTTP(response, request)
+
+		if !store.cancelled {
+			t.Errorf("store was not told to cancel")
+		}
+
+	})
+
 }
